23a419ef617223381ac81d889750897c0cd2bf67
trinhkien94 <20121949@student.hut.edu.vn>
2015-11-30 08:41:10 +0000

    Add account activations

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "kieict.94@gmail.com"
+  layout 'mailer'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get 'login'   =>  'sessions#new'
   post 'login'  =>  'sessions#create'
   delete 'logout' =>'sessions#destroy'
+  resources :account_activations, only: [:edit]
   resources :users
 
  # root             'static_pages#home'
